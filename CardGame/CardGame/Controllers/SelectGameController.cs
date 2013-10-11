b3e608abd5b2e0494ae87486b89da2f20a7738f3
lhart3 <lhart3@uis.edu>
2013-10-11 11:39:47 -0500

    logon and gameselect views started

---
+++ CardGame/CardGame/Controllers/SelectGameController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CardGame.Controllers
+{
+    public class SelectGameController : Controller
+    {
+        //
+        // GET: /SelectGame/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
