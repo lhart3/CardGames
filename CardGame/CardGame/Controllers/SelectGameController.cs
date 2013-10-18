4ac8bae4b60c6bcd9b1f58e5cb3130a2d7f6eddd
Goodpasture <jgood5@uis.edu>
2013-10-17 21:42:00 -0500

    After Class 10/17

--- CardGame/CardGame/Controllers/SelectGameController.cs
+++ CardGame/CardGame/Controllers/SelectGameController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CardGame.GameLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace CardGame.Controllers
 {
     public class SelectGameController : Controller
     {
+        Game Game { get; set; }
         //
         // GET: /SelectGame/
 
