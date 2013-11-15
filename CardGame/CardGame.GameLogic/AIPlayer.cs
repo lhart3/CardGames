36728867cf60fdbeb70a0023dc5d28a7d68a4696
kbrow20 <pkdkb110@gmail.com>
2013-11-14 21:55:05 -0600

    AIStarted

---
+++ CardGame/CardGame.GameLogic/AIPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardGame.GameLogic
+{
+    public abstract class AIPlayer : Player
+    {
+        public abstract void PlayGame(Game game);
+    }
+}
---
+++ CardGame/CardGame.GameLogic/AIPlayerGames.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardGame.GameLogic
+{
+    class AIPlayerGames : AIPlayer
+    {
+        public override void PlayGame(Game game)
+        {
+
+        }
+    }
+}
